f6b22bff3212d95f80caaf3324a12013ee30dc30
umamahale <umagasmos@gmail.com>
2021-10-09 01:36:04 -0400

    first message

---
+++ Anagram.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Anagram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1 = "heater";
+            string str2 = " reheat";
+            char[] ch1 = str1.ToLower().ToCharArray();
+            char[] ch2 = str2.ToLower().ToCharArray();
+            Array.Sort(ch1);
+            Array.Sort(ch2);
+            string va1 = new string (ch1)
+                string va2 = new string (ch2)
+                if (va1==va2)
+            {
+                Console.WriteLine("Anagram");
+
+            }
+                else
+            {
+                Console.WriteLine("Not an Anagram");
+            }
+        }
+    }
+}
