ef19331a19c5239c551ae690a3c334f4e84949e5
umamahale <umagasmos@gmail.com>
2021-10-09 01:38:42 -0400

    first message

---
+++ PrimeNumber.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace PrimeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int st, en, cnt = 0;
+            Console.WriteLine("starting value");
+            st = int.Parse(Console.ReadLine());
+            Console.WriteLine("ending Probelm");
+            en = int.Parse(Console.ReadLine());
+            for (int i = 1; i < en; i++)
+            {
+                for (int j = 1; j < en; j++)
+                {
+                    if (i % j == 0)
+
+                    {
+                        cnt++;
+                    }
+
+                }
+                if (cnt == 2)
+                {
+                    Console.WriteLine(i);
+
+                }
+                cnt = 0;
+            }
+        }
+    }
+}
+    
+
+
+
+
